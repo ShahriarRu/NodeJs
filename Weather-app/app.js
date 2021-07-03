54994d0c38eeffe4f2d605f49254b3275f792b72
Shahriar Alif <shahriarcse26@gmail.com>
2021-07-04 00:25:58 +0600

    weather app command line argument added

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -2,19 +2,25 @@ const chalk = require("chalk");
 const geoCode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geoCode("Lalpur Natore", (error, data) => {
-  if (error) {
-    return console.log(error);
-  }
-  forecast(data.latitude, data.latitude, (error, forecastdata) => {
+const address = process.argv[2];
+console.log(address);
+if (!address) {
+  console.log(chalk.inverse.red("Please provide an lacation"));
+} else {
+  geoCode(address, (error, data) => {
     if (error) {
-      return console.log(chalk.inverse.red(error));
+      return console.log(error);
     }
-    console.log(chalk.inverse.yellow.("Location: " + data.location));
-    console.log(
-      chalk.inverse.green(
-        "Temperature: " + forecastdata.temperature + " dregree celcious"
-      )
-    );
+    forecast(data.latitude, data.latitude, (error, forecastdata) => {
+      if (error) {
+        return console.log(chalk.inverse.red(error));
+      }
+      console.log(chalk.inverse.yellow("Location: " + data.location));
+      console.log(
+        chalk.inverse.green(
+          "Temperature: " + forecastdata.temperature + " dregree celcious"
+        )
+      );
+    });
   });
-});
+}
